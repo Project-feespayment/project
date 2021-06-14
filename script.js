06c5db83076dd9312a5a7dbe9730b44e2b4a93c5
KavyaDevadiga <kdevadiga74@gmail.com>
2021-06-14 10:02:53 +0530

    passcodeunmask

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ function openRegister(){
 function openLogin(){
     card.style.transform = "rotateY(0deg)";
 }
+/***********function for register password unmask */
+function passVisible1() {
+   var x=document.querySelectorAll(".passcode");
+   x.forEach(element => {if (element.type === "password") {
+    element.type = "text";
+  } else {
+    element.type = "password";
+  }   
+   });
+  }
+/***********function for login password unmask */
+  function passVisible(){
+      var x= document.querySelector(".passcode1")
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+  }
